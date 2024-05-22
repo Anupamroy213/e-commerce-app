b0236b224f0c449e1aa15eef7271fbb04f150099
Anupamroy213 <anupa.mroy213@gmail.com>
2024-05-22 14:49:26 +0530

    cors policy

--- server/server.js
+++ server/server.js
@@ -10,6 +10,26 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 require('./db');
+
+const cors = require('cors');
+
+const allowedOrigins = ['https://e-commerce-app-bay-seven.vercel.app/'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    // Allow requests with no origin (like mobile apps, curl, etc)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+
+
+
 app.use('/api', require("./Routes/DisplayData"));
 app.use('/api', require("./Routes/OrderData"));
 // app.use('/api', require("./Routes/MyOrderData"));
